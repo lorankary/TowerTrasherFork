7fe13d308f925cd04cbd330c297300a07e947dfa
Loran Kary <lk@focalpnt.com>
2020-04-05 15:29:18 -0700

    update server.js, add later.html
    An example of how one might limit websocket connections

--- server.js
+++ server.js
@@ -3,8 +3,32 @@ const expressWs = require('express-ws');    // load the express-ws module
 
 let wss = expressWs(express()); // websocket server
 let app = wss.app;  // express app
+let clients = wss.getWss().clients; // the websocket server's set of clients
 
-let DATA = {};  // all the players data, keyed by playerId
+let DATA = {};          // all the players data, keyed by playerId
+let maxConnections = 2; // the maximum number of client websocket connections
+                        // that we want to allow at a time
+
+// express middleware to potentially redirect the http request
+function later(req,resp,next) {
+    let readyClients = 0;
+    // Some client websocket connections may be closed but
+    // still in the set so don't count them.
+    clients.forEach(function(client) {
+        if (client.readyState === 1)
+            readyClients++;
+    });
+    // If we already have the maximum number of websocket connections
+    if(readyClients >= maxConnections) {
+        req.url = 'later.html';     // redirect the http request
+        }
+    next(); // continue to the express static middleware
+}
+
+// If the request is for "/" or "/index.html" and there are already
+// maxConnections, redirect the request
+app.get("/", later);
+app.get("/index.html", later);
 
 // serve all the static (client) files in the public folder over http
 app.use(express.static("./public"));
@@ -41,7 +65,7 @@ app.ws("/ws", function(ws, req) {
 
 // Every 15ms push all the player data to each of the clients
 setInterval(function() {
-    wss.getWss().clients.forEach(function(client) {
+    clients.forEach(function(client) {
         if (client.readyState === 1) {
             client.send(JSON.stringify({
                 type: "allPlayerData",
